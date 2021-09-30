0b7062b989022da7a35094c02ad530eeeb90f16c
harsha-akronym <harsha.kj89@gmail.com>
2021-10-01 02:03:37 +0800

    cleaned sensor domain tests

--- src/test/java/codes/harsha/co2stat/domain/SensorTest.java
+++ src/test/java/codes/harsha/co2stat/domain/SensorTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
