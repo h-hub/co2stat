4a9b8dc5eb6082eaaf4fe3d6af6393c4a0460d47
harsha-akronym <harsha.kj89@gmail.com>
2021-10-01 01:45:30 +0800

    update sensor controller tests

--- src/test/java/codes/harsha/co2stat/adapter/in/SensorControllerTest.java
+++ src/test/java/codes/harsha/co2stat/adapter/in/SensorControllerTest.java
@@ -3,6 +3,8 @@ package codes.harsha.co2stat.adapter.in;
 import codes.harsha.co2stat.application.port.in.CreateSensor;
 import codes.harsha.co2stat.application.port.in.GetSensorStatus;
 import codes.harsha.co2stat.application.port.in.ListAlerts;
+import codes.harsha.co2stat.domain.Alert;
+import codes.harsha.co2stat.domain.Sensor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +14,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
@@ -54,6 +60,29 @@ public class SensorControllerTest {
 
         assertEquals(actualResponseBody, expectedResponseBody);
 
+    }
+
+    @Test
+    public void when_getAlerts_shouldReturnJson() throws Exception {
+        Sensor sensor = new Sensor("ABC");
+        String date = "2021-10-01T01:40:32+08:00";
+        Alert alert = new Alert(sensor, ZonedDateTime.parse(date), ZonedDateTime.parse(date), new int[]{1,2,3});
+        List<Alert> alerts = new ArrayList<>();
+        alerts.add(alert);
+
+        when(listAlerts.getAlerts("ABC"))
+                .thenReturn(alerts);
+
+        MvcResult mvcResult = mvc.perform(
+                        get("/api/v1/sensors/ABC/alerts")).
+                andExpect(status().isOk()).
+                andReturn();
+
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+
+        String expectedResponseBody = "[{\"startTime\":\"2021-10-01T01:40:32+08:00\",\"endTime\":\"2021-10-01T01:40:32+08:00\",\"mesurement1\":1," +
+                "\"mesurement2\":2,\"mesurement3\":3}]";
+        assertEquals(actualResponseBody, expectedResponseBody);
     }
 
 }
